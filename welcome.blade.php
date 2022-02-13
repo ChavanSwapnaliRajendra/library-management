cd86d885d5b4a03cd6d1b78e05b8edf13d49d1ea
ChavanSwapnaliRajendra <97238594+ChavanSwapnaliRajendra@users.noreply.github.com>
2022-02-13 11:29:28 +0530

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,40 @@
+@extends('headers.header')
+@section('content')
+		<div class="main-banner" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-6 align-self-center">
+              <div class="owl-carousel owl-banner">
+                
+                <div class="item header-text">
+                  <h6>Library Management</h6>
+                  <h2>"A library <em>is not a</em> luxury <span>but one of the necessities of life."</span></h2>
+                  <p>You are NOT allowed to redistribute this template ZIP file on any Free CSS collection websites. Contact us for more info. Thank you.</p>
+                  <div class="down-buttons">
+                    <div class="main-blue-button-hover">
+                      <a href="">Our Services</a>
+                    </div>
+                    <div class="call-button">
+                      <a href=""><i class="fa fa-phone"></i> 090-080-0760</a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+  
+ 
+@endsection	
+
+
+    
